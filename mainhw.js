7273d9b6f0cb9e355ecae7047747aabe0e2ac79b
YLubinets <lubinec2@gmail.com>
2020-11-17 13:56:39 +0200

    4-5
    4-5

---
+++ mainhw.js
@@ -0,0 +1,26 @@
+    'use strict'
+
+   function createNewUser () {
+         newUser.firstName = prompt('Name');
+         newUser.lastName = prompt('Surname');
+         newUser.birthday = prompt(`dd.mm.yyyy`);
+
+}
+    const newUser = {
+        getLogin() {
+            console.log((this.firstName[0] + this.lastName.trim()).toLowerCase())
+        },
+        getAge () {
+            const year = (new Date().getFullYear())
+            console.log(`Age: ${year - this.birthday.slice(6)}`)
+            },
+        getPassword () {
+            console.log((this.firstName[0] + this.lastName + this.birthday.slice(6).trim()).toLowerCase())
+        }
+
+    }
+
+    createNewUser()
+    newUser.getLogin()
+    newUser.getAge()
+    newUser.getPassword()
