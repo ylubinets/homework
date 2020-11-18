4f9d7a4664240bff2bb5f9af0e7684a5de04d4fe
ylubinets <lubinec2@gmail.com>
2020-11-18 19:53:03 +0200

    n
    n

--- mainhw.js
+++ mainhw.js
@@ -1,26 +1,26 @@
-    'use strict'
+'use strict'
 
-   function createNewUser () {
-         newUser.firstName = prompt('Name');
-         newUser.lastName = prompt('Surname');
-         newUser.birthday = prompt(`dd.mm.yyyy`);
+function createNewUser () {
+      newUser.firstName = prompt('Name');
+      newUser.lastName = prompt('Surname');
+      newUser.birthday = prompt(`dd.mm.yyyy`);
 
 }
-    const newUser = {
-        getLogin() {
-            console.log((this.firstName[0] + this.lastName.trim()).toLowerCase())
-        },
-        getAge () {
-            const year = (new Date().getFullYear())
-            console.log(`Age: ${year - this.birthday.slice(6)}`)
-            },
-        getPassword () {
-            console.log((this.firstName[0] + this.lastName + this.birthday.slice(6).trim()).toLowerCase())
-        }
+ const newUser = {
+     getLogin() {
+         console.log((this.firstName[0] + this.lastName.trim()).toLowerCase())
+     },
+     getAge () {
+         const year = (new Date().getFullYear())
+         console.log(`Age: ${year - this.birthday.slice(6)}`)
+         },
+     getPassword () {
+         console.log(((this.firstName[0]).toUpperCase() + (this.lastName + this.birthday.slice(6)).toLowerCase()).trim())
+     }
 
-    }
+ }
 
-    createNewUser()
-    newUser.getLogin()
-    newUser.getAge()
-    newUser.getPassword()
+ createNewUser()
+ newUser.getLogin()
+ newUser.getAge()
+ newUser.getPassword()
