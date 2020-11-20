317697d54e87584d7c35cfca7578450f39339062
Ylubinets <lubinec2@gmail.com>
2020-11-20 11:57:30 +0200

    ew
    ew

--- mainhw.js
+++ mainhw.js
@@ -1,25 +1,45 @@
+'use strict'
 
-function createNewUser () {
-      newUser.firstName = prompt('Name');
-      newUser.lastName = prompt('Surname');
-      newUser.birthday = prompt(`dd.mm.yyyy`);
+    function createNewUser () {
+        const newUser = {
+            firstName : prompt('Name'),
+            lastName : prompt('Surname'),
+            birthday : prompt('dd.mm.yyyy'),
 
-}
- const newUser = {
-     getLogin() {
-         console.log((this.firstName[0] + this.lastName.trim()).toLowerCase())
-     },
-     getAge () {
-         const year = (new Date().getFullYear())
-         console.log(`Age: ${year - this.birthday.slice(6)}`)
-         },
-     getPassword () {
-         console.log(((this.firstName[0]).toUpperCase() + (this.lastName + this.birthday.slice(6)).toLowerCase()).trim())
-     }
+            getLogin() {
+                return ((this.firstName[0] + this.lastName.trim()).toLowerCase());
+            },
+            getAge () {
+                let today = new Date();
+                let age = today.getFullYear() - this.birthday.slice(6);
 
- }
+                if (today.getMonth() > this.birthday.slice(3,5)){
+                        return age;
+                    } if (today.getDate() > this.birthday.slice(0,3)){
+                        return age;
+                }else {
+                  return  age = age - 1;
+                }
+            },
+            getPassword () {
+                return (((this.firstName[0]).toUpperCase() + (this.lastName + this.birthday.slice(6)).toLowerCase()).trim());
+            },
+        }
+        return newUser;
+    }
 
- createNewUser()
- newUser.getLogin()
- newUser.getAge()
- newUser.getPassword()
+    const firstUser = createNewUser();
+
+    console.log(firstUser.getLogin());
+    console.log(firstUser.getAge());
+    console.log(firstUser.getPassword());
+
+
+
+
+
+
+
+
+
+
