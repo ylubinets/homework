e5d72913df16080ebf32a6a1324ac6809c045dd6
Ylubinets <lubinec2@gmail.com>
2020-12-04 21:42:18 +0200

    13
    13

--- HW JS 7 UPD/main.js
+++
@@ -1,8 +0,0 @@
-function displayList(array, parentDom = document.body) {
-    const elements = array.map((item) => {
-        return `<li>${item}</li>`;
-    });
-    parentDom.innerHTML = `<ul>${elements.join(" ")}</ul>`;
-}
-let arr = ["Одесса", "Киев", "Харьков", "Луцк", "Львов", "Симферополь", "Херсон", "Полтава"];
-displayList(arr, document.querySelector('#content'));
---
+++ HW JS13/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const btn = document.querySelector('.button');
+const body = document.getElementsByTagName('body');
+
+    if (localStorage.getItem('bgcolor') === 'salmon') {
+        body[0].classList.toggle('salmon');
+    } else {
+        body[0].classList.remove('salmon')
+    }
+
+
+    btn.addEventListener('click', () => {
+        body[0].classList.toggle('salmon');
+
+        if (body[0].classList.contains('salmon')){
+            localStorage.setItem('bgcolor', 'salmon');
+        } else {
+            localStorage.setItem('bgcolor', 'white')
+        }
+    })
+
