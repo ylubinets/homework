9d206bbaa27065fc377b62277e8692baf0acd273
YLubinets <lubinec2@gmail.com>
2021-01-31 12:30:04 +0200

    Upd HW

--- adaptive page gulp/src/js/script.js
+++ adaptive page gulp/src/js/script.js
@@ -1,5 +1,7 @@
+"use strict";
 document.getElementById("trigger").onclick = function () {
   document.getElementById("menu").classList.toggle("show");
   document.querySelector(".fa-times").classList.toggle("show");
   document.querySelector(".fa-bars").classList.toggle("show");
 };
+
