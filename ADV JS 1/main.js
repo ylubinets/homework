7efc814f4bb167c913fffb831ed51246fc3ed0d5
YLubinets <lubinec2@gmail.com>
2021-02-03 00:19:27 +0200

    Up

---
+++ ADV JS 1/main.js
@@ -0,0 +1,154 @@
+// 1
+const clients1 = ["Гилберт", "Сальваторе", "Пирс", "Соммерс", "Форбс", "Донован", "Беннет"];
+const clients2 = ["Пирс", "Зальцман", "Сальваторе", "Майклсон"];
+
+const clientsMain = [...clients1, ...clients2.filter(n => clients1.indexOf(n) === -1)];
+
+// 2
+
+const characters = [
+    {
+        name: "Елена",
+        lastName: "Гилберт",
+        age: 17,
+        gender: "woman",
+        status: "human"
+    },
+    {
+        name: "Кэролайн",
+        lastName: "Форбс",
+        age: 17,
+        gender: "woman",
+        status: "human"
+    },
+    {
+        name: "Аларик",
+        lastName: "Зальцман",
+        age: 31,
+        gender: "man",
+        status: "human"
+    },
+    {
+        name: "Дэймон",
+        lastName: "Сальваторе",
+        age: 156,
+        gender: "man",
+        status: "vampire"
+    },
+    {
+        name: "Ребекка",
+        lastName: "Майклсон",
+        age: 1089,
+        gender: "woman",
+        status: "vempire"
+    },
+    {
+        name: "Клаус",
+        lastName: "Майклсон",
+        age: 1093,
+        gender: "man",
+        status: "vampire"
+    }
+];
+
+// 2
+
+const charactersShortInfo = characters.map(char => {
+    const {name, lastName, age} = char;
+    return {name, lastName, age};
+});
+
+// 3
+
+const user1 = {
+    name: "John",
+    years: 30
+};
+
+const {name, years: age, isAdmin: isAdmin = false } = user1;
+
+// 4
+
+const satoshi2020 = {
+    name: 'Nick',
+    surname: 'Sabo',
+    age: 51,
+    country: 'Japan',
+    birth: '1979-08-21',
+    location: {
+        lat: 38.869422,
+        lng: 139.876632
+    }
+}
+
+const satoshi2019 = {
+    name: 'Dorian',
+    surname: 'Nakamoto',
+    age: 44,
+    hidden: true,
+    country: 'USA',
+    wallet: '1A1zP1eP5QGefi2DMPTfTL5SLmv7DivfNa',
+    browser: 'Chrome'
+}
+
+const satoshi2018 = {
+    name: 'Satoshi',
+    surname: 'Nakamoto',
+    technology: 'Bitcoin',
+    country: 'Japan',
+    browser: 'Tor',
+    birth: '1975-04-05'
+}
+
+
+const fullProfile = {...satoshi2018, ...satoshi2019, ...satoshi2020};
+
+// 5
+
+const books = [{
+    name: 'Harry Potter',
+    author: 'J.K. Rowling'
+}, {
+    name: 'Lord of the rings',
+    author: 'J.R.R. Tolkien'
+}, {
+    name: 'The witcher',
+    author: 'Andrzej Sapkowski'
+}];
+
+const bookToAdd = {
+    name: 'Game of thrones',
+    author: 'George R. R. Martin'
+}
+
+
+const booksFull = [
+    ...books, ...new Set([bookToAdd])
+]
+
+// 6
+
+const employee = {
+    name: 'Vitalii',
+    surname: 'Klichko'
+}
+
+const profile = {
+    ...employee,
+    age: 43,
+    salary: 2
+}
+
+
+// 7
+
+const array = ['value', () => 'showValue'];
+
+    [value, showValue] = array;
+
+alert(value); // должно быть выведено 'value'
+alert(showValue());  // должно быть выведено 'showValue'
+
+
+
+
